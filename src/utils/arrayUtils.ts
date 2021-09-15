1474ac7ab7271ab028a0f262b176ceb0a620196c
DmytroMyhal <dimamygal0708@gmail.com>
2021-09-15 16:43:39 +0300

    chore: separated Modal component from AccessLimiter

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -4,21 +4,6 @@ export const findStringInArray = (array: Array<string>, string: string): number 
   return foundItem ? array.indexOf(foundItem) : -1;
 };
 
-export const getRandomItemsFromArray = (arr: Array<any>, amount: number) => {
-  const result = new Array(amount);
-  const taken = new Array(arr.length);
-  let len = arr.length;
-
-  if (amount > len) return arr;
-
-  let itemsLeft = amount;
-  while (itemsLeft) {
-    len -= 1;
-    const x = Math.floor(Math.random() * len);
-    result[amount] = arr[x in taken ? taken[x] : x];
-    taken[x] = len in taken ? taken[len] : len;
-    itemsLeft -= 1;
-  }
-
-  return result;
-};
+export const getRandomItemsFromArray = (arr: Array<any>, amount: number) => arr
+  .sort(() => 0.5 - Math.random())
+  .slice(0, amount);
