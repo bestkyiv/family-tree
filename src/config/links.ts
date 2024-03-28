56759205d695546222b91aa1e7c4027837c57676
Mykyta Abramenko <mykyta.abramenko@plus4u.net>
2024-03-28 03:01:42 +0200

    Updates

--- src/config/links.ts
+++
@@ -1,4 +0,0 @@
-const checkIfBestieEndpoint = 'https://bg0lx1fae5.execute-api.eu-central-1.amazonaws.com/default/best-members-link';
-const adminTelegramUrl = 'https://t.me/dimamyhal';
-
-export { checkIfBestieEndpoint, adminTelegramUrl };
--- src/utils/arrayUtils.ts
+++
@@ -1,9 +0,0 @@
-export const findStringInArray = (array: Array<string>, string: string): number => {
-  const regexp = new RegExp(`^${string}.*`, 'i');
-  const foundItem = array.find((item) => regexp.test(item.toLowerCase()));
-  return foundItem ? array.indexOf(foundItem) : -1;
-};
-
-export const getRandomItemsFromArray = (arr: Array<any>, amount: number) => arr
-  .sort(() => 0.5 - Math.random())
-  .slice(0, amount);
