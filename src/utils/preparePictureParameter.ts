d5f9789154c620688e1fd019b3b233c3fc08624e
yevhenii.novikov <zhenyanovikov.dev@gmail.com>
2024-10-14 04:09:33 +0300

    update preparePicture

--- src/utils/preparePictureParameter.ts
+++ src/utils/preparePictureParameter.ts
@@ -2,7 +2,7 @@ export const preparePictureParameter = (picture?: string): string | undefined =>
   if (!picture) return undefined;
 
   // workaround for loading images, see more at https://justin.poehnelt.com/posts/google-drive-embed-images-403/
-  return picture.replace('drive.google.com', 'drive.lienuc.com');
+  return picture.replace(/drive\.google\.com\/file\/d\/([^\/?]+)(?:\/[^?]*)?.*/, 'gdrive-proxy.best-kyiv.org/$1');
 };
 
 export default preparePictureParameter;
