a3ac549a934b74c6925a1b487f6c8d347475a7db
yevhenii.novikov <zhenyanovikov.dev@gmail.com>
2024-10-14 04:55:52 +0300

    fix build issue

--- src/utils/preparePictureParameter.ts
+++ src/utils/preparePictureParameter.ts
@@ -1,12 +1,8 @@
-import cookie from 'react-cookies';
-
 export const preparePictureParameter = (picture?: string): string | undefined => {
   if (!picture) return undefined;
 
-  const cookieSpreadsheetId = cookie.load('spreadsheetId');
-
   // workaround for loading images, see more at https://justin.poehnelt.com/posts/google-drive-embed-images-403/
-  return picture.replace(/drive\.google\.com\/file\/d\/([^\/?]+)(?:\/[^?]*)?.*/, 'gdrive-proxy.best-kyiv.org/$1')+"?api_key="+cookieSpreadsheetId;
+  return picture.replace('drive.google.com', 'drive.lienuc.com');
 };
 
 export default preparePictureParameter;
